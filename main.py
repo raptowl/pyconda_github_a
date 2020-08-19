c05ca51dfd6b8b57adb87f12756fcf7f725853c8
raptowl <katsumi.furusho@gmail.com>
2020-08-19 13:33:17 +0000

    check mod1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import mymodule.mod1
 
 
 if __name__ == '__main__':
@@ -8,3 +9,15 @@ if __name__ == '__main__':
     parser.add_argument('-b', '--num_b', help='number b',
                         type=int, required=True)
     args = parser.parse_args()
+
+    a = args.num_a
+    b = args.num_b
+    adder1 = mymodule.mod1.NormalMyAdder(a, b)
+    adder2 = mymodule.mod1.ListMyAdder(a, b)
+    adder3 = mymodule.mod1.NumpyMyAdder(a, b)
+    print(f'{adder1.add()}')
+    print(f'{type(adder1.add())}')
+    print(f'{adder2.add()}')
+    print(f'{type(adder2.add())}')
+    print(f'{adder3.add()}')
+    print(f'{type(adder3.add())}')
